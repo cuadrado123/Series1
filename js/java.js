d5debff44fea225180d068bf21ef7b3567a99d84
cuadrado123 <cuatova7@gmail.com>
2017-11-14 10:17:44 -0300

    series

--- js/java.js
+++ js/java.js
@@ -1,16 +1,3 @@
-function accion(pag) {
-    $('#tabAccion'  ).load(pag);
-    }
-
-function ficcion(pag) {
-    $('#tabFiccion'  ).load(pag);
-    }
-
-
 function cargar(pag) {
-    $('#tabComedia'  ).load(pag);
-    }
-
-function inicio(pag) {
-    $('#tabInicio'  ).load(pag);
+    $('#tabSeccion'  ).load(pag);
     }
